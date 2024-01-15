6889f48a836baef1ae5147bc452c1589f0c4bf37
Swarnava Karan <swarnavakaran2001@gmail.com>
2024-01-15 18:46:36 +0530

    1st commit

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://swarnava:Skaran1234@task.guavg7a.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
